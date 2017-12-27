5b89e84a7869764a0e0342729bf92480d1f573a4
USER <105021057@live.asia.edu.tw>
2017-12-27 12:02:23 +0800

    1227_105021057

--- Dome2/connect.php
+++ Dome2/connect.php
@@ -20,11 +20,11 @@ if($id != null && $pw != null && $row[1] == $id && $row[2] == $pw)
     //將帳號寫入session，方便驗證使用者身份
     $_SESSION['username'] = $id;
     echo '登入成功!>.O';
-    echo '<meta http-equiv=REFRESH CONTENT=5;url=hw.html>';
+    echo '<meta http-equiv=REFRESH CONTENT=1;url=hw.html>';
 }
 else
 {
     echo '登入失敗!';
-    echo '<meta http-equiv=REFRESH CONTENT=1;url=login.php>';
+    echo '<meta http-equiv=REFRESH CONTENT=5;url=login.php>';
 }
 ?>
